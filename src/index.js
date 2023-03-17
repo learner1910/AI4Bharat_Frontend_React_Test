19f86f6d76ab015d7cc36826cc96317352419bcc
Toshini <66977351+learner1910@users.noreply.github.com>
2023-03-17 19:18:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { ReactDOM}  from 'react-redux';
-import store from './store';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
